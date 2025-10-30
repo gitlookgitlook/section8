b8ec9a1e46b01ae1973aaae5cf156e9e6ed19aed
eunjinim <eunjee2058@gmail.com>
2025-10-30 22:28:57 +0900

    오타 수정

--- src/page/ProductDetail.jsx
+++ src/page/ProductDetail.jsx
@@ -9,7 +9,7 @@ const ProductDetail = () => {
   const [product, setProduct] = useState(null);
 
   const getProductDetail = async () => {
-    let url = `https://my-json-sever.typicode.com/gitlookgitlook/section8/products/${id}`;
+    let url = `https://my-json-server.typicode.com/gitlookgitlook/section8/products/${id}`;
     let response = await fetch(url);
     let data = await response.json();
     // console.log("dtail data?", data);
